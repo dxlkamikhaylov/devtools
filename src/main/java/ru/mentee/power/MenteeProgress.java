fc59b8da716c984a7e4fe20d46df0275a9f4e78c
Daniil Mikhaylov <danya_mikhaylov_81@mail.ru>
2026-09-25 22:06:30 +0300

    docs: add Javadoc for MenteProgress class and methods

--- src/main/java/ru/mentee/power/MenteeProgress.java
+++ src/main/java/ru/mentee/power/MenteeProgress.java
@@ -1,9 +1,30 @@
 package ru.mentee.power;
 
+/**
+ * Прогресс менти в рамках спринта.
+ *
+ * @param menteeName имя менти
+ * @param sprintNumber номер спринта
+ * @param plannedHoursPerWeek запланированное количество часов в неделю
+ */
+
 public record MenteeProgress(String menteeName, int sprintNumber, int plannedHoursPerWeek) {
+
+  /**
+   * Проверяет, готов ли менти к спринту.
+   *
+   * @return {@code true}, если запланировано не менее 3 часов в неделю
+   */
+
   public boolean readyForSprint() {
     return plannedHoursPerWeek >= 3;
   }
+
+  /**
+   * Формирует краткое описание прогресса менти.
+   *
+   * @return строка в формате "Спринт %d -> %s: planned %d h"
+   */
 
   public String summary() {
     return "Спринт %d -> %s: planned %d h".formatted(sprintNumber, menteeName, plannedHoursPerWeek);
